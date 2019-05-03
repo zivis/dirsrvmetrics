a576136be654e594c39956784547d65c15fd36b0
Jonathan Buch <jbuch@synyx.de>
2019-05-03 15:01:43 +0200

    Add main.go with a plan

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+  // flags for ldap access
+  // access ldap
+  // parse cn=monitor
+  // parse cn=snmp,cn=monitor
+  // parse cn=counters,cn=monitor
+  // parse cn=monitor connection metrics
+  // publish to telegraf
+}
