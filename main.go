9ba9cc78d3d83b20210e7d92cbf9a2503d49a0f0
Sascha Rüssel <ruessel@synyx.de>
2019-07-04 10:15:58 +0200

    add InsecureSkipVerify to StartTLS connect

--- main.go
+++ main.go
@@ -21,12 +21,14 @@ var host = flag.String("host", "ldap://localhost:389", "Server URL")
 var user = flag.String("user", "scott", "Bind User")
 var password = flag.String("password", "", "User Password")
 var base = flag.String("base", "cn=Monitor", "Base for metrics")
+var tlsskipverify = flag.Bool("tlsskipverify", false, "skip verify at STARTTLS connect")
 
 type Config struct {
-  Host     string `json:"host"`
-  User     string `json:"user"`
-  Password string `json:"password"`
-  Base     string `json:"base"`
+  Host          string `json:"host"`
+  User          string `json:"user"`
+  Password      string `json:"password"`
+  Base          string `json:"base"`
+  Tlsskipverify bool `json:"tlsskipverify"`
 }
 
 func LoadConfiguration(file string) Config {
@@ -55,6 +57,9 @@ func main() {
     if config.Password != "" {
       password = &config.Password
     }
+    if config.Tlsskipverify {
+      tlsskipverify = &config.Tlsskipverify
+    }
   }
 
 	u, err := url.Parse(*host)
@@ -72,7 +77,18 @@ func main() {
 	}
 	defer l.Close()
 
-	err = l.StartTLS(&tls.Config{ServerName:u.Hostname()})
+	tlsconfig := &tls.Config{
+		ServerName:u.Hostname(),
+	}
+
+	if *tlsskipverify {
+		tlsconfig = &tls.Config{
+			ServerName:u.Hostname(),
+			InsecureSkipVerify: true,
+		}
+	}
+
+	err = l.StartTLS(tlsconfig)
 	if err != nil {
 		log.Println("Could not connect via STARTTLS")
 	}
